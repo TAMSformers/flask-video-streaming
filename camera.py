4dfad4f6db1c2c2ecfef0d43ba73917f24644b75
Floyd Horng <fwhorng@gmail.com>
2015-01-15 16:37:39 -0600

    added single stream

--- camera.py
+++ camera.py
@@ -3,10 +3,10 @@ from time import time
 
 class Camera(object):
     """An emulated camera implementation that streams a repeated sequence of
-    files 1.jpg, 2.jpg and 3.jpg at a rate of one frame per second."""
+    files stream.jpg, 2.jpg and 3.jpg at a rate of one frame per second."""
 
     def __init__(self):
-        self.frames = [open(f + '.jpg', 'rb').read() for f in ['1', '2', '3']]
+        pass
 
     def get_frame(self):
-        return self.frames[int(time()) % 3]
+        return open('stream.jpg', 'rb').read()
